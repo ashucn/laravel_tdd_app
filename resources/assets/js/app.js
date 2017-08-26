ab97253765312c2475a70386c6ef0bf1a6c18009
ashu <ashucn@qq.com>
2017-08-26 14:18:45 -0700

    UE update: submit loading

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,7 +9,6 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -18,7 +17,6 @@ window.Vue = require('vue');
 
 /*
 Vue.component('example', require('./components/Example.vue'));
-
 const app = new Vue({
     el: '#app'
 });
--- resources/assets/js/components/EventRegister.vue
+++ resources/assets/js/components/EventRegister.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="EventRegister__wrapper">
-        <button class="btn m-t-15" :class="mode" @click="handleRegister">{{text}}</button>
+        <button class="btn m-t-15" :class="mode" @click="handleRegister" :disabled="is_submitting">{{text}}</button>
         <span class="badge badge-default m-t-20" style="float: right !important;">{{count}}</span><br>
     </div>
 </template>
@@ -11,25 +11,33 @@
     export default {
         props: ['text', 'mode', 'eventId', 'count'],
         data() {
-            return {}
+            return {
+                origin_text:'',
+                is_submitting: false
+            }
         },
         methods: {
             handleRegister() {
                 console.log(this.eventId)
+                this.is_submitting = true
+                this.origin_text = this.text
+                this.text = 'Submitting'
                 let postDate = {
                     eventId: this.eventId
                 }
                 axios.post(registerUrl, postDate)
                     .then(response => {
                         toastr.options.positionClass = "toast-bottom-right";
+                        this.is_submitting = false
                         if (response.status == 200) {
                             this.count = response.data.type == 1 ? Number(this.count) - 1 : Number(this.count) + 1;
-                            toastr.success(this.text + ' the event successfully!!')
+                            toastr.success(this.origin_text + ' the event successfully!!')
                             this.mode = this.mode == 'btn-danger' ? 'btn-success' : 'btn-danger'
-                            this.text = this.text == 'Register' ? 'De-Register' : 'Register'
+                            this.text = this.origin_text == 'Register' ? 'De-Register' : 'Register'
                         }
                         else {
-                            toastr.error(this.text + ' the event failed...')
+                            this.text = this.origin_text
+                            toastr.error(this.origin_text + ' the event failed...')
                         }
                     })
             }
